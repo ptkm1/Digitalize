f0a670ea90fa22dfab12dd654a8b69583b51efdf
Patrick Rocha <ptkdev@outlook.com>
2021-01-11 14:58:40 -0300

    Logica e funcionalidades de crud dos documentos funcionando, integrado com leitor de código de barras

--- src/telas/VerDocumentos/index.tsx
+++ src/telas/VerDocumentos/index.tsx
@@ -1,40 +1,60 @@
-import { AntDesign } from '@expo/vector-icons';
-import { BarCodeScanner } from 'expo-barcode-scanner';
-import React, { useEffect, useState } from 'react';
-import { Button, Image, Modal, StyleSheet, Text } from 'react-native';
-import { BotãoOpcoes } from '../../components/Botoes';
-import { Container } from './style';
-
+import { AntDesign } from '@expo/vector-icons'
+import { BarCodeScanner } from 'expo-barcode-scanner'
+import SvgUri from 'expo-svg-uri'
+import React, { useEffect, useState } from 'react'
+import { Button, Image, Modal, StyleSheet, Text, View } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { BotãoOpcoes } from '../../components/Botoes'
+import api from '../../services/ConexaoApi'
+import { Descriçao, Header } from '../Documentos/style'
+import { Container } from './style'
 
 const VerDocumentos: React.FC = () => {
 	// TRATANDO O MODAL PARA ABRIR O LEITOR DE CODIGO DE BARRAS
-	const [modalVisible, setModalVisible] = useState(false);
-	const [hasPermission, setHasPermission] = useState<any>(null);
+	const [modalVisible, setModalVisible] = useState(false)
+	const [hasPermission, setHasPermission] = useState<any>(null)
 
-  const imagem = 'https://cdn.dribbble.com/users/332742/screenshots/14889756/media/f2d97d6dffb43d92623e091088d978d5.png?compress=1&resize=800x600';
+	const [DadosCidadao, setDadosCidadao] = useState<any>()
 
 	useEffect(() => {
 		(async () => {
-			const { status } = await BarCodeScanner.requestPermissionsAsync();
-			setHasPermission(status === 'granted');
-		})();
-	}, []);
+			const { status } = await BarCodeScanner.requestPermissionsAsync()
+			setHasPermission(status === 'granted')
+		})()
+	}, [])
 
-	const [scanned, setScanned] = useState(false);
-	const [barcode, setBarcode] = useState(null);
+	const [scanned, setScanned] = useState(false)
+	const [barcode, setBarcode] = useState(null)
 
 	const CodigoEscaneado = ({ type, data }) => {
-		setScanned(true);
-		alert(`Codigo de barras com o tipo ${type} e conteudo ${data} escaneado com sucesso`);
-		setBarcode(data);
-	};
+		setScanned(true)
+		alert(`Codigo de barras com o tipo ${type} e conteudo ${data} escaneado com sucesso`)
+		setBarcode(data)
+	}
+
+	useEffect(() => {
+		async function BuscarCidadao() {
+			try {
+				const { data } = await api.post('/cidadao/documentos', { barcode: barcode })
+
+				return setDadosCidadao(data)
+			} catch (error) {
+				console.log('nao foi possivel encontrar dados')
+			}
+		}
+
+		BuscarCidadao()
+	}, [barcode])
+
+	console.log(DadosCidadao)
 
 	// Verificando se foi permitido ou não o uso da camera
 	if (hasPermission === null) {
-		return <Text>Requesting for camera permission</Text>;
+		return <Text>Requesting for camera permission</Text>
 	}
 	if (hasPermission === false) {
-		return <Text>No access to camera</Text>;
+		return <Text>No access to camera</Text>
 	}
 
 	return (
@@ -49,7 +69,7 @@ const VerDocumentos: React.FC = () => {
 					<BotãoOpcoes
 						style={{ zIndex: 15 }}
 						onPress={() => {
-							setModalVisible(!modalVisible);
+							setModalVisible(!modalVisible)
 						}}
 					>
 						<Text>FecharModal </Text>
@@ -65,15 +85,46 @@ const VerDocumentos: React.FC = () => {
 				{scanned && <Button title={'Escanear de novo'} onPress={() => setScanned(false)} />}
 			</Modal>
 
-			{barcode ? (
-          <>
-          <Image style={{ width: 200, height: 200 }} source={{uri:imagem}} />
-          <Text>Cidadão Achado</Text>
-        </>
+      <Header>
+				<SvgUri width="200" height="200" source={require('../../../assets/Logo.svg')} />
+			</Header>
+
+			<Descriçao>Digitalize de forma simples a identidade do cidadão</Descriçao>
+
+			{barcode && DadosCidadao ? (
+				<SafeAreaView style={{ height: '80%', width: '100%' }}>
+					<ScrollView showsVerticalScrollIndicator={false} style={{paddingBottom: '60%'}}>
+						<View style={{ borderWidth: 1, margin: 5 }}>
+							<Image
+								style={{ width: "100%", height: 300, resizeMode: 'cover' }}
+								source={{ uri: DadosCidadao.certidao_nascimento }}
+							/>
+							<Text>Certidão de nascimento</Text>
+						</View>
+						<View style={{ borderWidth: 1, margin: 5 }}>
+							<Image style={{ width: "100%", height: 300, resizeMode: 'cover' }} source={{ uri: DadosCidadao.nome_social }} />
+							<Text>Nome social</Text>
+						</View>
+						<View style={{ borderWidth: 1, margin: 5 }}>
+							<Image
+								style={{ width: "100%", height: 300, resizeMode: 'cover' }}
+								source={{ uri: DadosCidadao.tipagem_sanguinea }}
+							/>
+							<Text>Tipagem sanguinea</Text>
+						</View>
+						<View style={{ borderWidth: 1, margin: 5, marginBottom: '50%' }}>
+							<Image
+								style={{ width: "100%", height: 300, resizeMode: 'cover' }}
+								source={{ uri: DadosCidadao.relatorio_medico }}
+							/>
+							<Text>Relatório Médico</Text>
+						</View>
+					</ScrollView>
+				</SafeAreaView>
 			) : (
 				<BotãoOpcoes
 					onPress={() => {
-						setModalVisible(!modalVisible);
+						setModalVisible(!modalVisible)
 					}}
 				>
 					<AntDesign name="barcode" size={33} color="black" />
@@ -81,7 +132,7 @@ const VerDocumentos: React.FC = () => {
 				</BotãoOpcoes>
 			)}
 		</Container>
-	);
-};
+	)
+}
 
-export default VerDocumentos;
+export default VerDocumentos
